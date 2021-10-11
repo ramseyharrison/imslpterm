60983a91c4b7567e509615e5f91fe46395698f7f
Ramsey Harrison <ramseyharrison@pop-os.localdomain>
2021-10-11 14:01:03 -0400

    composers.json

--- main.py
+++ main.py
@@ -1,46 +1,23 @@
 import os
 import json
 import json_helpers
-
-from imslp import client
+import mwclient
+from imslp.client import ImslpClient
+from imslp.interfaces import scraping
 
 DIRPATH = os.path.abspath(os.path.dirname(__file__))
 IMSLP_JSON_PATH = DIRPATH + '/imslp_json/{composer}.json'
 LOCAL_JSON_PATH = DIRPATH + '/local_json/{composer}.json'
 LIST_JSON_PATH = DIRPATH + '/composers.json'
 
-client = client.ImslpClient
-
 
 #Main method for getting and storing IMSLP composer information
 def process_composer(composer):
-    fetch_imslp_json(composer) #calls IMSLP package
-    cleaned_compositions = []
-    compositions = json_helpers.read_json(
-        IMSLP_JSON_PATH.format(composer=composer))
-    compositions.sort(key=lambda x: x['intvals']['worktitle']) #sorts by worktitle name
-
-    for index in range(len(compositions)):
-        #stores desired information from IMSLP package search 
-        #cat would be the official catalog number of composition, if it exists(not intval icatno)
-        #for purpose of url_images see get_images()
-        cleaned_compositions.append(
-            {
-                'id':   index,
-                'title': compositions[index]['intvals']['worktitle'],
-                'permlink': compositions[index]['permlink'],
-                'cat': "",
-                'url_images': []
-            }
-        )
-
-    json_helpers.interact_with_json_file(LOCAL_JSON_PATH.format(
-        composer=composer),
-        "w",
-        lambda f: f.write(json.dumps(cleaned_compositions)))
+    pass
 
 
 def fetch_imslp_json(composer):
+    client = ImslpClient()
     PATH = IMSLP_JSON_PATH
 
     works = list(client.search_works(composer=composer))
@@ -69,21 +46,47 @@ def get_local_composition(composer_id, composition_id):
 #invokes IMSLP package
 #name must be properly formatted IMSLP category name
 
-def add_new_composer(name):
+def add_new_composer(composer):
     PATH = LIST_JSON_PATH  
+   
+    fetch_imslp_json(composer) #calls IMSLP package
+    cleaned_compositions = []
+    compositions = json_helpers.read_json(
+        IMSLP_JSON_PATH.format(composer=composer))
+    compositions.sort(key=lambda x: x['intvals']['worktitle']) #sorts by worktitle name
+
+    for index in range(len(compositions)):
+        #stores desired information from IMSLP package search 
+        #cat would be the official catalog number of composition, if it exists(not intval icatno)
+        #for purpose of url_images see get_images()
+        cleaned_compositions.append(
+            {
+                'id':   index,
+                'title': compositions[index]['intvals']['worktitle'],
+                'permlink': compositions[index]['permlink'],
+                'cat': "",
+                'url_images': []
+            }
+        )
+
+    json_helpers.interact_with_json_file(LOCAL_JSON_PATH.format(
+        composer=composer),
+        "w",
+        lambda f: f.write(json.dumps(cleaned_compositions)))
+
     composers = json_helpers.read_json(PATH)
     composers.append(
         {
             "id": len(composers), #local ID for composer
-            "name": name,
+            "name": composer,
         }
     )
     json_helpers.interact_with_json_file(
         PATH,
         "w",
         lambda f: f.write(json.dumps(composers)))
-    process_composer(name)
 
+add_new_composer("Debussy, Claude")
 #add_new_composer("Brahms, Johannes")
 
 
@@ -96,3 +99,17 @@ def get_images(composition):
     link = composition['permlink']
     #client.get_images(link) 
     pass
+
+def get_name_from_permlink(permlink):
+    return permlink[permlink.rindex('/')+1]
+
+def get_image_metadata(composition):
+    site = mwclient.Site('imslp.org',path='/') 
+    page = mwclient.page.Page(site,name=get_name_from_permlink(composition['permlink']))
+    images = scraping.fetch_images_metadata(page)
+    for image in images:
+        print(image)
+    # with open("test.pdf",'wb') as f:
+    #     images[0]['obj'].download(f)
+    
+get_image_metadata(get_local_composition(1,10))
